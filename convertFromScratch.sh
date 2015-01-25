85647ef1cd8bd35e57227d27699e26d89134260a
bgundlach <gundlachcomputing@gmail.com>
2015-01-25 01:04:50 -0700

    added woff & woff2 support

--- convertFromScratch.sh
+++ convertFromScratch.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=`echo $0 | sed "s/convertFromScratch.sh//"`
 
-$SCRIPT_DIR/convertFonts.sh --clean
+$SCRIPT_DIR/css3fonts --clean
 rm hinted-*.ttf
-$SCRIPT_DIR/convertFonts.sh --use-font-weight --output=weighted-stylesheet.css  $*
-$SCRIPT_DIR/convertFonts.sh --use-font-weight --use-font-prefix=hinted- --autohint  --output=hinted-stylesheet.css *.ttf
+$SCRIPT_DIR/css3fonts --use-font-weight --output=weighted-stylesheet.css  $*
+$SCRIPT_DIR/css3fonts --use-font-weight --use-font-prefix=hinted- --autohint  --output=hinted-stylesheet.css *.ttf
